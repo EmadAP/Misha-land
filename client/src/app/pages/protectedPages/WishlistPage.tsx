632dcfb3ebe6aa8271ba273bb8cc4e0245a7a6c3
EmadAP <emad.amoupour1997@gmail.com>
2026-01-03 10:15:46 +0330

    fix/protected-pages-wish-lisy-page

--- client/src/app/pages/protectedPages/WishlistPage.tsx
+++ client/src/app/pages/protectedPages/WishlistPage.tsx
@@ -1,12 +1,11 @@
 import ProductItemCard from "@/components/sections/ProductItemCard";
 import { ProductDummyData } from "@/lib/mock/dummylists";
-import React from "react";
 
 function WishlistPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
-      {ProductDummyData.map((item) => (
-        <ProductItemCard key={item.id} item={item} />
+      {ProductDummyData.map((product) => (
+        <ProductItemCard key={product.id} product={product} />
       ))}
     </div>
   );
